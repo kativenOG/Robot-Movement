b2d58c98299a52f06a0245ddeacc59db16767165
mattias <mattias.trettel@studenti.unitn.it>
2022-04-29 13:49:22 +0200

    ingresso p2p = 6 angoli

---
+++ ur5.h
@@ -0,0 +1,48 @@
+#pragma once
+//#include <iostream>
+//#include <cmath>
+//#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+namespace robot{
+    class ur5{
+        private:
+
+            // DH parameters 
+            const float a[6] = {0,-0.425,-0.3922,0,0,0}; 
+            const float d[6] = {0.1625,0,0,0.1333,0.0997,0.0996}; 
+
+
+            // Matrici per direct kinematics 
+            Matrix4f T10f;
+            Matrix4f T21f;
+            Matrix4f T32f;
+            Matrix4f T43f;
+            Matrix4f T54f;
+            Matrix4f T65f;
+
+            // Setters Handlers:
+            void setT10f(float th1);
+            void setT21f(float th2);
+            void setT32f(float th3);
+            void setT43f(float th4);
+            void setT54f(float th5);
+            void setT65f(float th6);
+
+            Matrix3f eul2rotm(Vector3f& v);
+            Vector3f rotm2eul(Matrix3f& m);
+
+        public:
+
+            // Direct kinematics function:
+            void ur5direct(float th[6], Vector3f &x, Matrix3f &r); 
+
+            // Direct kinematics function:
+            MatrixXf ur5inverse(Vector3f eep, Matrix3f eer);
+
+            void p2pMotionPlan(RowVectorXf& qEs, Vector3f& xEf, Vector3f& phiE0, float minT, float maxT, float deltaT,MatrixXf& Th_1, MatrixXf& xE_1, MatrixXf& phiE_1);
+
+    };
+}; 
