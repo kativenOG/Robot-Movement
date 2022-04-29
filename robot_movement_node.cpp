8479f7c8686feb30577740c0ad35d9e2a21a5837
kativenOG <mangrella.andrea@gmail.com>
2022-04-29 11:47:34 +0200

    publishing e subscribing ai joint del ur5 con roscpp

---
+++ robot_movement_node.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include "ros/ros.h"
+#include <ros/package.h>
+#include "std_msgs/Float64.h"
+#include "control_msgs/JointControllerState.h"
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+
+
+//vector<control_msgs::JointControllerState::ConstPtr> initial_jnt_pos;
+int initial_jnt_pos[7];
+
+void shoulder_pan_getter(const control_msgs::JointControllerState::ConstPtr& val){
+    initial_jnt_pos[0]= val->process_value;
+}
+void shoulder_lift_getter(const control_msgs::JointControllerState::ConstPtr& val){
+    initial_jnt_pos[1]=val->process_value;
+}
+void elbow_getter(const control_msgs::JointControllerState::ConstPtr& val){
+    initial_jnt_pos[2]=val->process_value;
+}
+void wrist_1_getter(const control_msgs::JointControllerState::ConstPtr& val){
+    initial_jnt_pos[3]=val->process_value;
+}
+void wrist_2_getter(const control_msgs::JointControllerState::ConstPtr& val){
+    initial_jnt_pos[4]=val->process_value;
+}
+void wrist_3_getter(const control_msgs::JointControllerState::ConstPtr& val){
+    initial_jnt_pos[5]=val->process_value;
+}
+void gripper_getter(const control_msgs::JointControllerState::ConstPtr& val){
+    initial_jnt_pos[6]=val->process_value;
+}
+
+
+int main(int argc,char ** argv){
+
+    // Creo il nodo 
+    ros::init(argc,argv,"gatto_node");
+    // Creo un nodo interno al nodo principale
+    ros::NodeHandle n;
+    // Creo un array per il publishing, ha una casella per ogni joint
+    ros::Publisher ur5_joint_array_pub[7];
+
+    // lego l'array ai vari topic, in cui pubblicheranno i valori di posizione      
+    ur5_joint_array_pub[0] = n.advertise<std_msgs::Float64>("/shoulder_pan_joint_position_controller/command",1000);
+    ur5_joint_array_pub[1] = n.advertise<std_msgs::Float64>("/shoulder_lift_joint_position_controller/command",1000);
+    ur5_joint_array_pub[2] = n.advertise<std_msgs::Float64>("/elbow_joint_position_controller/command",1000);
+    ur5_joint_array_pub[3] = n.advertise<std_msgs::Float64>("/wrist_1_joint_position_controller/command",1000);
+    ur5_joint_array_pub[4] = n.advertise<std_msgs::Float64>("/wrist_2_joint_position_controller/command",1000);
+    ur5_joint_array_pub[5] = n.advertise<std_msgs::Float64>("/wrist_3_joint_position_controller/command",1000);
+    ur5_joint_array_pub[6] = n.advertise<std_msgs::Float64>("/gripper_controller/command",1000);
+
+    // creo subscriber che ascoltano nei topic di poszione dei joint, e si salvano la loro poszione nello spazio tramite dei wrapper :) 
+    ros::Subscriber shoulder_pan_joint_sub = n.subscribe("",1000,shoulder_pan_getter); 
+    ros::Subscriber shoulder_lift_joint_sub = n.subscribe("",1000,shoulder_lift_getter); 
+    ros::Subscriber elbow_joint_sub = n.subscribe("",1000,elbow_getter); 
+    ros::Subscriber wrist_1_joint_sub = n.subscribe("",1000,wrist_1_getter); 
+    ros::Subscriber wrist_2_joint_sub = n.subscribe("",1000,wrist_2_getter); 
+    ros::Subscriber wrist_3_joint_sub = n.subscribe("",1000,wrist_3_getter); 
+    ros::Subscriber left_knucle_joint_sub = n.subscribe("",1000,gripper_getter); 
+
+    return 0;
+}
+
+
