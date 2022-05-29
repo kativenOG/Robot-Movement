489161cf6bab59e8c2de5d2f44b2ce9149ee10f9
cancianilorenzo <lorenzo.canciani@studenti.unitn.it>
2022-05-29 23:58:23 +0200

    cambiamenti per attaccare blocco

--- movement_include/pick&place.h
+++ movement_include/pick&place.h
@@ -1,13 +1,16 @@
 #pragma once
-
-Vector3f STND_POS;
-STND_POS << -0.4064, -0.1403, 0.5147;
-Vector3f STND_ANGLE;
-STND_ANGLE << -0.4280, -0.0028, 3.0650;
+#include "gazebo_ros_link_attacher/Attach.h"
+#include "move.h"
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
 
 void take(ros::ServiceClient attach, ros::Publisher ur5_pub[], Eigen::Vector3f vf, Eigen::Vector3f phiF, Eigen::MatrixXf Th, Eigen::VectorXf initial_pos, char *blockName, robot::ur5 u, ros::Rate loop_rate)
 {
-
+    Vector3f STND_POS;
+    STND_POS << -0.4064, -0.1403, 0.5147;
+    Vector3f STND_ANGLE;
+    STND_ANGLE << -0.4280, -0.0028, 3.0650;
+    
     gazebo_ros_link_attacher::Attach srv;
     movement(ur5_pub, vf, phiF, Th, initial_pos, u, loop_rate);
 
@@ -40,8 +43,8 @@ void place(ros::ServiceClient detach, ros::Publisher ur5_pub[], Eigen::Vector3f 
 
 void take_and_place(ros::ServiceClient attach, ros::ServiceClient detach, ros::Publisher ur5_pub[], Eigen::Vector3f vf1, Eigen::Vector3f vf2, Eigen::Vector3f phiF, Eigen::MatrixXf Th, Eigen::VectorXf initial_pos, char *blockName, robot::ur5 u, ros::Rate loop_rate)
 {
-    
+
     take(attach, ur5_pub[], vf1, phiF, Th, initial_pos, blockName, u, loop_rate);
-    sleep(1)
+    sleep(1);
     place(detach, ur5_pub[], vf2, phiF, Th, initial_pos, blockName, u, loop_rate);
 };
