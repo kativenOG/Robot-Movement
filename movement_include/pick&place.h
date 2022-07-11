93beccb6e84fa94f157471f04bd0f0c5a2bcb5c5
mattias <mattias.trettel@studenti.unitn.it>
2022-07-11 11:58:06 +0200

    1

--- movement_include/pick&place.h
+++ movement_include/pick&place.h
@@ -18,7 +18,6 @@ void cleanTh(Eigen::MatrixXf &Th)
 void  closeGripper(ros::Publisher gripper,float size)
 {
     std_msgs::Float64 temp;
-    // CAMBIATO A FISSO 1 
     temp.data = size;
     gripper.publish(temp);
 };
@@ -34,7 +33,6 @@ void take(ros::ServiceClient attach, ros::Publisher ur5_pub[], Eigen::Vector3f v
 {
 
     STND_POS << 0, 0.3203, 0.6147;
-    //STND_ANGLE << -0.4280, -0.0028, 3.0650;
     STND_ANGLE << 0, 3.14, 0;
 
     gazebo_ros_link_attacher::Attach srv;
@@ -42,23 +40,21 @@ void take(ros::ServiceClient attach, ros::Publisher ur5_pub[], Eigen::Vector3f v
 
     Vector3f above_step;
     above_step=vf;
-    //above_step[2] = above_step[2]+0.5;
     above_step[2] = above_step[2]+0.1;
-    std::cout << phiF << "\n";
     movement(ur5_pub, above_step, phiF, Th, initial_pos, u, loop_rate);
     int rows = Th.rows() - 1;
     for (int i = 0; i < 6; i++) vv[i] = Th(rows, i + 1);
     cleanTh(Th);
     //-------------------------
-    movement(ur5_pub, vf, phiF, Th, vv, u, loop_rate);
+    /*movement(ur5_pub, vf, phiF, Th, vv, u, loop_rate);
     rows = Th.rows() - 1;
     for (int i = 0; i < 6; i++) vv[i] = Th(rows, i + 1);
     cleanTh(Th);
     int y=1;
     float x;
-    //phiF(0)=0;
-    //phiF(1)=3.14;
-    //phiF(2)=0;
+    phiF(0)=0;
+    phiF(1)=3.14;
+    phiF(2)=0;
     while(y==1){
         std::cout << "pos?";
         std::cin >> y;
@@ -103,7 +99,7 @@ void take(ros::ServiceClient attach, ros::Publisher ur5_pub[], Eigen::Vector3f v
         std::cout << "phief=" << phiF << "\n";
         std::cout << "continuare?";
         std::cin >> y;
-    }
+    }*/
     //-------------------------
     movement(ur5_pub, vf, phiF, Th, vv, u, loop_rate);
 
@@ -130,21 +126,14 @@ void place(ros::ServiceClient detach, ros::Publisher ur5_pub[], Eigen::Vector3f 
     STND_ANGLE << -0.4280, -0.0028, 3.0650;
 
     gazebo_ros_link_attacher::Attach srv;
-    //*movement(ur5_pub, vf, phiF, Th, initial_pos, u, loop_rate);
 
     int rows = Th.rows() - 1;
     VectorXf vv(6);
-    //*for (int i = 0; i < 6; i++) vv[i] = Th(rows, i + 1);
-    //*std::cout<<"vv place1:  "<<vv<<std::endl;
-    // for (int i = 0; i < 6; i++) v[i] = Th(rows, i + 1);
-
-    //*cleanTh(Th);
-    //*sleep(1.5);
+    
     Vector3f ffangle;
     ffangle<<u.legoAngle[blockNumber][0],u.legoAngle[blockNumber][1],u.legoAngle[blockNumber][2];
-    //movement(ur5_pub, vf, ffangle , Th, vv, u, loop_rate);
     movement(ur5_pub, vf, ffangle , Th, initial_pos, u, loop_rate);
-    sleep(1.5);
+    sleep(2.5);
     openGripper(gripper);
 
     srv.request.model_name_1 = "ur5";
@@ -165,10 +154,7 @@ void place(ros::ServiceClient detach, ros::Publisher ur5_pub[], Eigen::Vector3f 
 void take_and_place(ros::ServiceClient attach, ros::ServiceClient detach, ros::Publisher ur5_pub[], Eigen::Vector3f vf1, Eigen::Vector3f vf2, Eigen::Vector3f phiF, Eigen::MatrixXf Th, Eigen::VectorXf initial_pos, char *blockName,int blockNumber, robot::ur5 u, ros::Rate loop_rate,ros::Publisher gripper,float gripperValue)
 {
     STND_POS << 0, 0.3203, 0.6147;
-    //-----------------------
-    //std::cout << blockNumber << "\n";
-    //vf1(0)=vf1(0)+0.024;
-    //vf1(1)=vf1(1)+0.011;
+
     vf1(2)=vf1(2)-0.002;
     float indx[11] = {-0.43,-0.344,-0.258,-0.172,-0.086,0,0.086,0.172,0.258,0.344,0.43};
     float indy[11] = {-0.2,-0.255,-0.31,-0.365,-0.42,-0.475,-0.53,-0.585,-0.64,-0.695,-0.75};
@@ -197,20 +183,7 @@ void take_and_place(ros::ServiceClient attach, ros::ServiceClient detach, ros::P
         {0.224, 0.279, 0.333, 0.388, 0.44, 0.494, 0.548, 0.602, 0.657, 0.711, 0.764},
         {0.225, 0.279, 0.333, 0.389, 0.442, 0.496, 0.55, 0.604, 0.658, 0.713, 0.767},
         {0.225, 0.28, 0.334, 0.39, 0.443, 0.497, 0.551, 0.606, 0.66, 0.715, 0.772}};
-    /*float block0x[7][7] = {{-162,-172,-167,-163,-173,-168,-164},
-                           {-107,-103,-113,-108,-104,-114,-109},
-                           {-38,-48,-44,-40,-54,-45,-41},
-                           {15,6,10,15,19,9,13},
-                           {70,60,65,69,59,64,68},
-                           {128,129,119,124,128,118,123},
-                           {194,184,188,192,183,187,191}};
-    float block0y[7][7] = {{170,115,47,-8,-63,-118,-186},
-                           {174,106,51,-4,-59,-113,-182},
-                           {179,110,41,-14,-49,-123,-178},
-                           {169,114,45,-10,-64,-119,-188},
-                           {173,119,50,-5,-60,-114,-168},
-                           {178,109,54,-15,-55,-124,-179},
-                           {168,113,44,-11,-64,-120,-189}};*/
+
     int i,j;
     float minx=-0.43;
     float maxx=0.43;
@@ -231,19 +204,6 @@ void take_and_place(ros::ServiceClient attach, ros::ServiceClient detach, ros::P
                 j=c;
             }
         }
-        if(Y<-0.695){
-            if(X<-0.344){
-                vf1(2)+=0.007;
-            }
-            if(X>0.344){
-                vf1(2)+=0.002;
-            }
-        }
-        //std::cout << "i=" << i << " j=" << j << " X range: " << indx[i] << "-" << indx[i+1] << " Y range: " << indy[j] << "-" << indy[j+1];
-        //std::cout << "\nMx=\n" << Mx[i][j]  << "-" << Mx[i][j+1]  << "\n" <<Mx[i+1][j]  << "-"<< Mx[i+1][j+1] << "\n";
-        //std::cout << "My=\n" << My[i][j]  << "-" << My[i][j+1]  << "\n" <<My[i+1][j]  << "-"<< My[i+1][j+1] << "\n";
-        //std::cout << "proporzione x:" << (X-indx[i])/(indx[i+1]-indx[i]);
-        //std::cout << "\nproporzione y:" << (Y-indy[j])/(indy[j+1]-indy[j]) << "\n";
         float dx1,dx2,dy1,dy2;
         dx1=Mx[i][j]+(Mx[i+1][j]-Mx[i][j])*(X-indx[i])/(indx[i+1]-indx[i]);
         dx2=Mx[i][j+1]+(Mx[i+1][j+1]-Mx[i][j+1])*(X-indx[i])/(indx[i+1]-indx[i]);
@@ -252,25 +212,11 @@ void take_and_place(ros::ServiceClient attach, ros::ServiceClient detach, ros::P
         dy1=My[i][j]+(My[i][j+1]-My[i][j])*(Y-indy[j])/(indy[j+1]-indy[j]);
         dy2=My[i+1][j]+(My[i+1][j+1]-My[i+1][j])*(Y-indy[j])/(indy[j+1]-indy[j]);
         vf1(1)=dy1+(dy2-dy1)*(X-indx[i])/(indx[i+1]-indx[i]);
-        /*
-        float dx,dx1,dx2,dy,dy1,dy2;
-        dx1=block0x[i][j]+(block0x[i+1][j]-block0x[i][j])*(X-(minx+0.1*i))/0.1;
-        dx2=block0x[i][j+1]+(block0x[i+1][j+1]-block0x[i][j+1])*(X-(minx+0.1*i))/0.1;
-        dx=dx1+(dx2-dx1)*(Y-(maxy-0.1*j))/0.1;
 
-        dy1=block0y[i][j]-(block0y[i][j+1]-block0y[i][j])*(Y-(maxy-0.1*j))/0.1;
-        dy2=block0y[i+1][j]-(block0y[i+1][j+1]-block0y[i+1][j])*(Y-(maxy-0.1*j))/0.1;
-        dy=dy1+(dy2-dy1)*(X-(maxy-0.1*j))/0.1;*/
-
-
-        //vf1(0)=vf1(0)+dx*0.0001;
-        //vf1(1)=vf1(1)+dy*0.0001;
         std::cout << "vf corretto " << vf1(0) << " " << vf1(1) << "\n";
     }
     int g;
-    std::cout << "iniziare?";
-    std::cin >> g;
-    //-----------------------
+
     take(attach, ur5_pub, vf1, phiF, Th, initial_pos, blockName, u, loop_rate,gripper, gripperValue);
 
     int rows = Th.rows() - 1;
