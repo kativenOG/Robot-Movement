260590158b14ede7fae7eae7bbe69d2a9b351cea
mangrella andrea <mangrella.andrea@gmail.com>
2022-07-22 14:22:40 +0200

    aumentato lo sleep per permettere di smettere di muoversi ai blocchetti appena spawnati

--- scripts/ultimate.sh
+++ scripts/ultimate.sh
@@ -7,24 +7,24 @@ if test $1 -eq 1
 then
   gnome-terminal -- ../spawners/spawner_punto1.py   
   gnome-terminal -- rosrun robot_movement point_2_node  
-  sleep 2 
+  sleep 8 
   gnome-terminal -- rosrun ultimate_vision ultimate_position.py   
 elif test $1 -eq 2        
 then
   gnome-terminal -- ../spawners/spawner_punto2.py  
   gnome-terminal -- rosrun robot_movement point_2_node 
-  sleep 2 
+  sleep 8 
   gnome-terminal -- rosrun ultimate_vision ultimate_position.py  
 elif test $1 -eq 3        
 then
   gnome-terminal -- ../spawners/spawner_punto3.py 
   gnome-terminal -- rosrun robot_movement point_3_node 
-  sleep 2 
+  sleep 8 
   gnome-terminal -- rosrun ultimate_vision ultimate_position.py  
 elif test $1 -eq 4        
 then
   gnome-terminal -- ../spawners/spawner_punto4.py  
   gnome-terminal -- rosrun robot_movement point_4_node 
-  sleep 2 
+  sleep 8 
   gnome-terminal -- rosrun ultimate_vision ultimate_position_point4.py  
 fi 
