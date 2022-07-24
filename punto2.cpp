8c8c8cde798e6e96c790bcd1378ffe97db5b1241
mangrella andrea <mangrella.andrea@gmail.com>
2022-07-24 18:37:22 +0200

    check angoli

--- punto2.cpp
+++ punto2.cpp
@@ -101,6 +101,8 @@ int main(int argc, char **argv)
 
         int ee_roll_appo = ((int)(block_angle(i,2)*1000))%10000;
         float ee_roll = ee_roll_appo/1000;
+        if(ee_roll<0.7) ee_roll=0;
+        else ee_roll=1.57;
         ee_angle<< block_angle(i,0),block_angle(i,1),ee_roll;
 
         MatrixXf Th;
